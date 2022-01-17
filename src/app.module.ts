dc5d5001bb8e4eeefe774fd31525740009c4244b
Kasir Barati <node.js.developers.kh@gmail.com>
2022-01-17 19:24:55 +0330

    add tasks resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import configuration from '@src/config';
 import { UserModule } from './user/user.module';
+import { TasksModule } from './tasks/tasks.module';
 
 @Module({
     imports: [
@@ -12,6 +13,7 @@ import { UserModule } from './user/user.module';
         }),
         AuthModule,
         UserModule,
+        TasksModule,
     ],
     controllers: [],
     providers: [],
